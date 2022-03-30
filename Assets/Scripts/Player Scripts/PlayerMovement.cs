e43db536e09ada462fd8a62b7bf8c1c416aa84cc
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-03-30 19:57:05 +0530

    Update PlayerMovement.cs

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -14,25 +14,34 @@ public class PlayerMovement : MonoBehaviour
      private Rigidbody2D myBody;
 
      private Vector2 moveVector;
+
+     private GameObject artifact;
      
      private SpriteRenderer sr;
+     private float harvestTimer;
+     private bool isHarvesting;
+     
 
      private void Awake()
      {    //get the rigidbody2d component
           myBody = GetComponent<Rigidbody2D>();
           sr = GetComponent<SpriteRenderer>();
      }
-          
-     }
 
-     private void OnEnable()
+     private void FixedUpdate()
      {
-          
-     }
+          if (isHarvesting)
+               myBody.velocity = Vector2.zero;
+          else
+          {
+               moveVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-     private void Start()
-     {
-          
+               if (moveVector.sqrMagnitude > 1)
+                    moveVector = moveVector.normalized;
+               
+               myBody.velocity = new Vector2(moveVector.x * movementSpeed, moveVector.y * movementSpeed);
+               
+          }
      }
 }
 
