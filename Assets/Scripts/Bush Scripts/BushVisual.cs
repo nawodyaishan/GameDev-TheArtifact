f1379f5268b16f4b2a9d5e616891b3b1fd1634a8
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-04-07 18:41:26 +0530

    Bushs and Fruit Script update

--- Assets/Scripts/Bush Scripts/BushVisual.cs
+++ Assets/Scripts/Bush Scripts/BushVisual.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class BushVisual : MonoBehaviour
 {
 
-    [SerializeField] private Sprite[] bushSprites, fruitSprites, drySprtites;
+    [SerializeField] private Sprite[] bushSprites, fruitSprites, drySprites;
 
     [SerializeField] private SpriteRenderer[] fruitsRenderers;
 
 
-    public enum BushVarient { Green, cyan, Yelow }
+    public enum BushVariant { Green, cyan, Yelow }
 
-    private BushVarient bushVarient;
+    private BushVariant bushVariant;
 
     public float hideTimePerFruit = 0.2f;
 
@@ -23,31 +23,60 @@ public class BushVisual : MonoBehaviour
     {
         sr = GetComponent<SpriteRenderer>();
 
-        bushVarient = (BushVarient)Random.Range(0, bushSprites.Length);
-        sr.sprite = bushSprites[(int)bushVarient];
+        bushVariant = (BushVariant)Random.Range(0, bushSprites.Length);
+        sr.sprite = bushSprites[(int)bushVariant];
 
         if (Random.Range(0, 2) == 1)
             sr.flipX = true;
 
         for (int i = 0; i < fruitsRenderers.Length; i++)
         {
-            fruitsRenderers[i].sprite = fruitSprites[(int)bushVarient];
+            fruitsRenderers[i].sprite = fruitSprites[(int)bushVariant];
             //fruitsRenderers[i].enabled = false;
         }
-
-
-
     }
 
-
-
-
-
-
-    private void Start()
+    public BushVariant GetBushVariant()
     {
-
-
+        return bushVariant;
     }
 
+    public void SetToDry()
+    {
+        sr.sprite = drySprites[(int)bushVariant];
+    }
+
+    IEnumerator _HideFruits(float time, int index) {
+
+        yield return new WaitForSeconds(time);
+        fruitsRenderers[index].enabled = false;
+        }
+
+    public void HideFruits()
+    {
+        float waitTimeForFruit = hideTimePerFruit;
+
+        for (int i = 0; i < fruitsRenderers.Length; i++)
+        {
+            
+
+            StartCoroutine(_HideFruits(waitTimeForFruit, i));
+            waitTimeForFruit += hideTimePerFruit;
+
+            
+        }  
+    }
+
+
+    public void ShowFruits()
+    {
+        for (int i = 0; i < fruitsRenderers.Length; i++)
+        {
+            fruitsRenderers[i].enabled = false;
+        }
+    }
 }
+
+
+
+
